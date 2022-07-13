45c75bfb8b1a25511f20fa1b43391c9b2239e8af
GeraldV3 <lopezgerald981@gmail.com>
2022-07-13 11:44:16 +0800

    I fixed my JsonFile to make it valid and readable in my code

--- Form1.cs
+++ Form1.cs
@@ -18,178 +18,136 @@ namespace Resume_Creator
 {
     public partial class MainForm : Form
     {
-        private readonly string path = @"C:\Users\pc\Desktop\Resume Creator\ResumeInformation.json";
         public MainForm()
         {
             InitializeComponent();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         }
         private void Read_BTN_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string jsonFromFile;
-                using (var readr = new StreamReader(path))
-                {
-                    jsonFromFile = readr.ReadToEnd();
-                }
-                ResumeInformation Resume = JsonSerializer.Deserialize<ResumeInformation>(jsonFromFile);
-                //Basic Information
-                string firstname = Resume.firstname;
-                string lastname = Resume.lastname;
-                string middlename = Resume.middlename;
-                string Email = Resume.email;
-                string phonenumber = Resume.phonenumber;
-                //Address
-                string homeaddress = Resume.homeaddress;
-                string postalcode = Resume.postalcode;
-                string municipality = Resume.municipality;
-                string province = Resume.province;
-                //Education
-                string college = Resume.college;
-                string courge = Resume.course;
-                string collegegraduated = Resume.collegegraduated;
-                string seniorhighschool = Resume.seniorhighschool;
-                string strand = Resume.strand;
-                string seniorhighschoolgraduated = Resume.seniorhighschoolgraduated;
-                string highschool = Resume.highschool;
-                //Achievement
-                string achievement = Resume.achievement;
-                string achievement1 = Resume.achievement1;
-                //Experience
-                string experience = Resume.experience;
-                string contain = Resume.contain;
-                string contain1 = Resume.contain1;
-                string contain2 = Resume.contain2;
-                //Skills
-                string skills = Resume.skills;
-                string skills1 = Resume.skills1;
-                string skills2 = Resume.skills2;
-                string skills3 = Resume.skills3;
-                string skills4 = Resume.skills4;
-                string skills5 = Resume.skills5;
-                //Hobby
-                string hobby = Resume.hobby;
-                string hobby1 = Resume.hobby1;
-                string hobby2 = Resume.hobby2;
-                string hobby3 = Resume.hobby3;
-
-                FirstName_LBL.Text = "First Name: " + Resume.firstname;
-                LastName_LBL.Text = "Last Name: " + Resume.lastname;
-                MiddleName_LBL.Text = "MiddleName: " + Resume.middlename;
-                Email_LBL.Text = "Email: " + Resume.email;
-                PhoneNumber_LBL.Text = "PhoneNumber: " + Resume.phonenumber;
-                HomeAddress_LBL.Text = "HomeAddress: " + Resume.homeaddress;
-                Postal_LBL.Text = "Postal: " + Resume.postalcode;
-                Municipality_LBL.Text = "Municipality: " + Resume.municipality;
-                Province_LBL.Text = "Provicne: " + Resume.province;
-                College_LBL.Text = "College: " + Resume.college;
-                Course_LBL.Text = "Course: " + Resume.course;
-                GraduatedYear_LBL.Text = "GreaduatedYear: " + Resume.collegegraduated;
-                SHS_LBL.Text = "SeniorHighSchool: " + Resume.seniorhighschool;
-                Strand_LBL.Text = "Strand: " + Resume.strand;
-                GraduatedYear1_LBL.Text = "GreaduatedYear: " + Resume.seniorhighschoolgraduated;
-                HS_LBL.Text = "High School: " + Resume.highschool;
-                GraduatedYear2_LBL.Text = "GraduatedYear: " + Resume.highschoolgraduated;
-                Achievement_LBL.Text = "Achievement: " + Resume.achievement;
-                Achievement1_LBL.Text = "Achievement: " + Resume.achievement1;
-                Experience_LBL.Text = "Exprience: " + Resume.experience;
-                Contain_LBL.Text = "Contain: " + Resume.contain;
-                Contain1_LBL.Text = "Contain: " + Resume.contain1;
-                Contain2_LBL.Text = "Contain: " + Resume.contain2;
-                Skills_LBL.Text = "Skills: " + Resume.skills;
-                Skills1_LBL.Text = "Skills: " + Resume.skills1;
-                Skills2_LBL.Text = "Skills: " + Resume.skills2;
-                Skills3_LBL.Text = "Skills: " + Resume.skills3;
-                Skills4_LBL.Text = "Skills: " + Resume.skills4;
-                Skills5_LBL.Text = "Skills: " + Resume.skills5;
-                Hobby_LBL.Text = "Hobby: " + Resume.hobby;
-                Hobby1_LBL.Text = "Hobby: " + Resume.hobby1;
-                Hobby2_LBL.Text = "Hobby: " + Resume.hobby2;
-                Hobby3_LBL.Text = "Hobby: " + Resume.hobby3;
-            }
-            catch
-            {
-
-            }
-        }
-        private void Generate_BTN_Click(object sender, EventArgs e)
-        {
-            string filename = @"C:\Users\pc\Desktop\Resume Creator\ResumeInformation.json";
-            string JSONFILE = File.ReadAllText(filename);
-            ResumeInformation Resume = JsonSerializer.Deserialize<ResumeInformation>(JSONFILE);
+            string filename = "ResumeInformation.json";
+            string jsonInfo = File.ReadAllText(filename);
+            ResumeInformation Resume = JsonSerializer.Deserialize<ResumeInformation>(jsonInfo);
+            MessageBox.Show(jsonInfo);
             //Basic Information
-            string firstname = Resume.firstname;
-            string lastname = Resume.lastname;
-            string middlename = Resume.middlename;
+            string Firstname = Resume.firstname;
+            string Lastname = Resume.lastname;
+            string Middlename = Resume.middlename;
             string Email = Resume.email;
-            string phonenumber = Resume.phonenumber;
+            string Phonenumber = Resume.phonenumber;
             //Address
-            string homeaddress = Resume.homeaddress;
-            string postalcode = Resume.postalcode;
-            string municipality = Resume.municipality;
-            string province = Resume.province;
+            string Homeaddress = Resume.homeaddress;
+            string Postalcode = Resume.postalcode;
+            string Municipality = Resume.municipality;
+            string Province = Resume.province;
             //Education
-            string college = Resume.college;
-            string courge = Resume.course;
-            string collegegraduated = Resume.collegegraduated;
-            string seniorhighschool = Resume.seniorhighschool;
-            string strand = Resume.strand;
-            string seniorhighschoolgraduated = Resume.seniorhighschoolgraduated;
-            string highschool = Resume.highschool;     
+            string College = Resume.college;
+            string Course = Resume.course;
+            string Collegegraduated = Resume.collegegraduated;
+            string Seniorhighschool = Resume.seniorhighschool;
+            string Strand = Resume.strand;
+            string Seniorhighschoolgraduated = Resume.seniorhighschoolgraduated;
+            string Highschool = Resume.highschool;
+            string Highschoolgraduated = Resume.highschoolgraduated;
             //Achievement
-            string achievement = Resume.achievement;
-            string achievement1 = Resume.achievement1;
+            string Achievement = Resume.achievement;
+            string Achievement1 = Resume.achievement1;
             //Experience
-            string experience = Resume.experience;
-            string contain = Resume.contain;
-            string contain1 = Resume.contain1;
-            string contain2 = Resume.contain2;
-            //Skills
-            string skills = Resume.skills;
-            string skills1 = Resume.skills1;
-            string skills2 = Resume.skills2;
-            string skills3 = Resume.skills3;
-            string skills4 = Resume.skills4;
-            string skills5 = Resume.skills5;
-            //Hobby
-            string hobby = Resume.hobby;
-            string hobby1 = Resume.hobby1;
-            string hobby2 = Resume.hobby2;
-            string hobby3 = Resume.hobby3;
-
-            FirstName_LBL.Text = "First Name: " + Resume.firstname;
+            string Experience = Resume.experience;
+            string Contain = Resume.contain;
+            string Contain1 = Resume.contain1;
+            string Contain2 = Resume.contain2;
+           // Skills
+            string Skills1 = Resume.skills1;
+            string Skills2 = Resume.skills2;
+            string Skills3 = Resume.skills3;
+            string Skills4 = Resume.skills4;
+            string Skills5 = Resume.skills5;
+            string Skills6 = Resume.skills6;
+           // Hobby
+            string Hobby1 = Resume.hobby1;
+            string Hobby2 = Resume.hobby2;
+            string Hobby3 = Resume.hobby3;
+            string Hobby4 = Resume.hobby4;
+            FirstName_LBL.Text = "First Name: " + Firstname;
             LastName_LBL.Text = "Last Name: " + Resume.lastname;
-            MiddleName_LBL.Text = "MiddleName: " + Resume.middlename;
+            MiddleName_LBL.Text = "Middle Name: " + Resume.middlename;
             Email_LBL.Text = "Email: " + Resume.email;
-            PhoneNumber_LBL.Text = "PhoneNumber: " + Resume.phonenumber;
-            HomeAddress_LBL.Text = "HomeAddress: " + Resume.homeaddress;
+            PhoneNumber_LBL.Text = "Phone Number: " + Resume.phonenumber;
+            HomeAddress_LBL.Text = "Home Address: " + Resume.homeaddress;
             Postal_LBL.Text = "Postal: " + Resume.postalcode;
             Municipality_LBL.Text = "Municipality: " + Resume.municipality;
-            Province_LBL.Text = "Provicne: " + Resume.province;
+            Province_LBL.Text = "Province: " + Resume.province;
             College_LBL.Text = "College: " + Resume.college;
             Course_LBL.Text = "Course: " + Resume.course;
-            GraduatedYear_LBL.Text = "GreaduatedYear: " + Resume.collegegraduated;
-            SHS_LBL.Text = "SeniorHighSchool: " + Resume.seniorhighschool;
+            GraduatedYear_LBL.Text = "GraduatedYear: " + Resume.collegegraduated;
+            SHS_LBL.Text = "Senior High School: " + Resume.seniorhighschool;
             Strand_LBL.Text = "Strand: " + Resume.strand;
-            GraduatedYear1_LBL.Text = "GreaduatedYear: " + Resume.seniorhighschoolgraduated;
+            GraduatedYear1_LBL.Text = "GraduatedYear: " + Resume.seniorhighschoolgraduated;
             HS_LBL.Text = "High School: " + Resume.highschool;
             GraduatedYear2_LBL.Text = "GraduatedYear: " + Resume.highschoolgraduated;
             Achievement_LBL.Text = "Achievement: " + Resume.achievement;
             Achievement1_LBL.Text = "Achievement: " + Resume.achievement1;
-            Experience_LBL.Text = "Exprience: " + Resume.experience;
+            Experience_LBL.Text = "Experience: " + Resume.experience;
             Contain_LBL.Text = "Contain: " + Resume.contain;
             Contain1_LBL.Text = "Contain: " + Resume.contain1;
             Contain2_LBL.Text = "Contain: " + Resume.contain2;
-            Skills_LBL.Text = "Skills: " + Resume.skills;
-            Skills1_LBL.Text = "Skills: " + Resume.skills1;
-            Skills2_LBL.Text = "Skills: " + Resume.skills2;
-            Skills3_LBL.Text = "Skills: " + Resume.skills3;
-            Skills4_LBL.Text = "Skills: " + Resume.skills4;
-            Skills5_LBL.Text = "Skills: " + Resume.skills5;
-            Hobby_LBL.Text = "Hobby: " + Resume.hobby;
-            Hobby1_LBL.Text = "Hobby: " + Resume.hobby1;
-            Hobby2_LBL.Text = "Hobby: " + Resume.hobby2;
-            Hobby3_LBL.Text = "Hobby: " + Resume.hobby3;
+            Skills_LBL.Text = "Skills: " + Resume.skills1;
+            Skills1_LBL.Text = "Skills: " + Resume.skills2;
+            Skills2_LBL.Text = "Skills: " + Resume.skills3;
+            Skills3_LBL.Text = "Skills: " + Resume.skills4;
+            Skills4_LBL.Text = "Skills: " + Resume.skills5;
+            Skills5_LBL.Text = "Skills: " + Resume.skills6;
+            Hobby_LBL.Text = "Hobby: " + Resume.hobby1;
+            Hobby1_LBL.Text = "Hobby: " + Resume.hobby2;
+            Hobby2_LBL.Text = "Hobby: " + Resume.hobby3;
+            Hobby3_LBL.Text = "Hobby: " + Resume.hobby4;
+        }
+        private void Generate_BTN_Click(object sender, EventArgs e)
+        {
+            string filename = "ResumeInformation.json";
+            string jsonInfo = File.ReadAllText(filename);
+            ResumeInformation Resume = JsonSerializer.Deserialize<ResumeInformation>(jsonInfo);
+            //Basic Information
+            string Firstname = Resume.firstname;
+            string Lastname = Resume.lastname;
+            string Middlename = Resume.middlename;
+            string Email = Resume.email;
+            string Phonenumber = Resume.phonenumber;
+            //Address
+            string Homeaddress = Resume.homeaddress;
+            string Postalcode = Resume.postalcode;
+            string Municipality = Resume.municipality;
+            string Province = Resume.province;
+            //Education
+            string College = Resume.college;
+            string Course = Resume.course;
+            string Collegegraduated = Resume.collegegraduated;
+            string Seniorhighschool = Resume.seniorhighschool;
+            string Strand = Resume.strand;
+            string Seniorhighschoolgraduated = Resume.seniorhighschoolgraduated;
+            string Highschool = Resume.highschool;
+            string Highschoolgraduated = Resume.highschoolgraduated;
+            //Achievement
+            string Achievement = Resume.achievement;
+            string Achievement1 = Resume.achievement1;
+            //Experience
+            string Experience = Resume.experience;
+            string Contain = Resume.contain;
+            string Contain1 = Resume.contain1;
+            string Contain2 = Resume.contain2;
+            // Skills
+            string Skills1 = Resume.skills1;
+            string Skills2 = Resume.skills2;
+            string Skills3 = Resume.skills3;
+            string Skills4 = Resume.skills4;
+            string Skills5 = Resume.skills5;
+            string Skills6 = Resume.skills6;
+            // Hobby
+            string Hobby1 = Resume.hobby1;
+            string Hobby2 = Resume.hobby2;
+            string Hobby3 = Resume.hobby3;
+            string Hobby4 = Resume.hobby4;
         }
     }
     public class ResumeInformation
@@ -223,16 +181,16 @@ namespace Resume_Creator
         public string contain1 { get; set; }
         public string contain2 { get; set; }
        // Skills
-        public string skills { get; set; }
         public string skills1 { get; set; }
         public string skills2 { get; set; }
         public string skills3 { get; set; }
         public string skills4 { get; set; }
         public string skills5 { get; set; }
+        public string skills6 { get; set; }
        // Hobby
-        public string hobby { get; set; }
         public string hobby1 { get; set; }
         public string hobby2 { get; set; }
         public string hobby3 { get; set; }
+        public string hobby4 { get; set; }
     }
 }
